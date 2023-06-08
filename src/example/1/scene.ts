d75171ec227c081f24523f6d65efeb737fd02095
lolicon <lovesyndra@gmail.com>
2023-06-08 15:41:39 +0800

    feat: apply monopo.vn main visual

--- src/example/1/scene.ts
+++ src/example/1/scene.ts
@@ -1,5 +1,5 @@
 import GUI from "lil-gui";
-import { Renderer, Program, Color, Mesh, Triangle } from "ogl-typescript";
+import { Renderer, Program, Mesh, Triangle } from "ogl-typescript";
 import LoaderManager from "src/managers/LoaderManager";
 
 import vertex from "./glsl/main.vert";
@@ -7,95 +7,100 @@ import fragment from "./glsl/main.frag";
 import { getCoverUV } from "../../utils/ogl";
 
 // tutorial: https://www.youtube.com/watch?v=kJpyUp_pQqU&ab_channel=HowToCodeThat%3F
+/*
+    原理主要是倚賴 displacement-map 這張圖上黑與白，
+    定義 displaceForce 取樣上面的紅色（rgb 其實都可以，主要只是要 0~1，畢竟是黑白就沒差）來決定位移距離的多寡，
+    決定誰先位移，進而達成扭曲破壞的效果
+*/
 
 class Scene {
-    renderer!: Renderer;
-    mesh!: Mesh;
-    program!: Program;
-    guiObj = { offset: 1 };
-    el!: HTMLElement;
+	renderer!: Renderer;
+	mesh!: Mesh;
+	program!: Program;
+	guiObj = { offset: 1 };
+	el!: HTMLElement;
 
-    constructor() {
-        this.setGUI();
-        this.setScene();
-        this.events();
-    }
+	constructor() {
+		this.setGUI();
+		this.setScene();
+		this.events();
+	}
 
-    setGUI() {
-        const gui = new GUI();
-        gui.add(this.guiObj, "offset", 0, 1).onChange(this.guiChange);
-    }
+	setGUI() {
+		const gui = new GUI();
+		gui.add(this.guiObj, "offset", 0, 1).onChange(this.guiChange);
+	}
 
-    async setScene() {
-        this.el = document.querySelector(".scene") as HTMLCanvasElement;
-        const canvas = document.querySelector(".scene-canvas") as HTMLCanvasElement;
-        this.renderer = new Renderer({ canvas, dpr: Math.min(window.devicePixelRatio, 2) });
-        const { gl } = this.renderer;
-        gl.clearColor(1, 1, 1, 1);
+	async setScene() {
+		this.el = document.querySelector(".scene") as HTMLCanvasElement;
+		const canvas = document.querySelector(".scene-canvas") as HTMLCanvasElement;
+		this.renderer = new Renderer({ canvas, dpr: Math.min(window.devicePixelRatio, 2) });
+		const { gl } = this.renderer;
+		gl.clearColor(1, 1, 1, 1);
 
-        this.handleResize();
+		this.handleResize();
 
-        const loaderManager = new LoaderManager();
+		const loaderManager = new LoaderManager();
 
-        await loaderManager.load(
-            [
-                {
-                    name: "image1",
-                    texture: "./1/image-1.jpg",
-                },
-                {
-                    name: "image2",
-                    texture: "./1/image-2.jpg",
-                },
-                {
-                    name: "displacement",
-                    texture: "./1/displacement-map.jpg",
-                },
-            ],
-            gl
-        );
+		await loaderManager.load(
+			[
+				{
+					name: "image1",
+					texture: "./1/image-1.jpg",
+				},
+				{
+					name: "image2",
+					texture: "./1/image-2.jpg",
+				},
+				{
+					name: "displacement",
+					texture: "./1/displacement-map.jpg",
+				},
+			],
+			gl
+		);
 
-        // ! 待修正，不知道為什麼texture的width height 抓不到，所以才先佔用image
-        const uvCover1 = getCoverUV(gl, loaderManager.assets.image1.image);
-        const uvCover2 = getCoverUV(gl, loaderManager.assets.image2.image);
+		// ! 待修正，不知道為什麼texture的width height 抓不到，所以才先佔用image
+		const uvCover1 = getCoverUV(gl, loaderManager.assets.image1.image);
+		const uvCover2 = getCoverUV(gl, loaderManager.assets.image2.image);
 
-        this.program = new Program(gl, {
-            vertex,
-            fragment,
-            uniforms: {
-                uOffset: { value: this.guiObj.offset },
-                uTexture1: { value: loaderManager.assets.image1 },
-                uRepeat1: { value: uvCover1.repeat },
-                uOffset1: { value: uvCover1.offset },
-                uTexture2: { value: loaderManager.assets.image2 },
-                uRepeat2: { value: uvCover2.repeat },
-                uOffset2: { value: uvCover2.offset },
-                uDisplacementTexture: { value: loaderManager.assets.displacement },
-            },
-        });
+		this.program = new Program(gl, {
+			vertex,
+			fragment,
+			uniforms: {
+				uOffset: { value: this.guiObj.offset },
+				uTexture1: { value: loaderManager.assets.image1 },
+				uRepeat1: { value: uvCover1.repeat },
+				uOffset1: { value: uvCover1.offset },
+				uTexture2: { value: loaderManager.assets.image2 },
+				uRepeat2: { value: uvCover2.repeat },
+				uOffset2: { value: uvCover2.offset },
+				uDisplacementTexture: { value: loaderManager.assets.displacement },
+			},
+		});
 
-        const geometry = new Triangle(gl);
+		const geometry = new Triangle(gl);
 
-        this.mesh = new Mesh(gl, { geometry, program: this.program });
-    }
+		this.mesh = new Mesh(gl, { geometry, program: this.program });
+	}
 
-    events() {
-        window.addEventListener("resize", this.handleResize, false);
-        requestAnimationFrame(this.handleRAF);
-    }
+	events() {
+		window.addEventListener("resize", this.handleResize, false);
+		requestAnimationFrame(this.handleRAF);
+	}
 
-    handleResize = () => {
-        this.renderer.setSize(this.el.offsetWidth, this.el.offsetHeight);
-    };
+	handleResize = () => {
+		this.renderer.setSize(this.el.offsetWidth, this.el.offsetHeight);
+	};
 
-    handleRAF = (t: number) => {
-        requestAnimationFrame(this.handleRAF);
-        if (this.mesh) this.renderer.render({ scene: this.mesh });
-    };
+	handleRAF = (t: number) => {
+		requestAnimationFrame(this.handleRAF);
+		if (this.mesh) this.renderer.render({ scene: this.mesh });
+	};
 
-    guiChange = (value: number) => {
-        this.program.uniforms.uOffset.value = value;
-    };
+	guiChange = (value: number) => {
+		this.program.uniforms.uOffset.value = value;
+	};
 }
 
 export default Scene;
---
+++ src/example/4/scene.ts
@@ -0,0 +1,153 @@
+import GUI from "lil-gui";
+import {
+	Mesh,
+	ShaderMaterial,
+	WebGLRenderer,
+	PerspectiveCamera,
+	Scene,
+	DirectionalLight,
+	AmbientLight,
+	Vector2,
+	DoubleSide,
+	TorusGeometry,
+	SphereGeometry,
+	WebGLCubeRenderTarget,
+	RGBAFormat,
+	LinearMipMapLinearFilter,
+	CubeCamera,
+	SRGBColorSpace,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
+
+import vertex from "./glsl/main.vert";
+import fragment from "./glsl/main.frag";
+import outerVertex from "./glsl/outer.vert";
+import outerFragment from "./glsl/outer.frag";
+
+import { DotScreenShader } from "./CustomShader";
+
+// tutorial: https://www.youtube.com/watch?v=kJpyUp_pQqU&ab_channel=HowToCodeThat%3F
+
+class BallScene {
+	renderer!: WebGLRenderer;
+	mesh!: Mesh;
+	mesh2!: Mesh;
+	guiObj = { offset: 1 };
+	el!: HTMLElement;
+	scene!: Scene;
+	camera!: PerspectiveCamera;
+	cubeCamera!: CubeCamera;
+	cubeRenderTarget!: WebGLCubeRenderTarget;
+	composer!: EffectComposer;
+	controls!: OrbitControls;
+
+	constructor() {
+		// this.setGUI();
+		this.setScene();
+		this.initPost();
+		this.events();
+		this.handleResize();
+	}
+
+	setGUI() {
+		const gui = new GUI();
+		// gui.add(this.guiObj, "offset", 0, 1).onChange(this.guiChange);
+	}
+
+	initPost() {
+		this.composer = new EffectComposer(this.renderer);
+		this.composer.addPass(new RenderPass(this.scene, this.camera));
+
+		const effect1 = new ShaderPass(DotScreenShader);
+		effect1.uniforms["scale"].value = 4;
+		this.composer.addPass(effect1);
+	}
+
+	setScene() {
+		this.el = document.querySelector(".scene") as HTMLDivElement;
+		this.renderer = new WebGLRenderer({ antialias: true });
+		this.el.appendChild(this.renderer.domElement);
+
+		this.scene = new Scene();
+		this.camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
+		this.camera.position.z = 1.5;
+
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+		const geometry = new SphereGeometry(2, 32, 32);
+		const material = new ShaderMaterial({
+			vertexShader: vertex,
+			fragmentShader: fragment,
+			side: DoubleSide,
+			uniforms: {
+				uTime: { value: 0 },
+				uResolution: { value: new Vector2() },
+				uDisplace: { value: 2 },
+				uSpread: { value: 1.2 },
+				uNoise: { value: 16 },
+			},
+		});
+
+		this.mesh = new Mesh(geometry, material);
+		this.scene.add(this.mesh);
+
+		this.cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+			format: RGBAFormat,
+			generateMipmaps: true,
+			minFilter: LinearMipMapLinearFilter,
+			// @ts-ignore
+			encoding: SRGBColorSpace,
+		});
+
+		this.cubeCamera = new CubeCamera(0.1, 10, this.cubeRenderTarget);
+
+		const geometry2 = new SphereGeometry(0.4, 32, 32);
+		const material2 = new ShaderMaterial({
+			vertexShader: outerVertex,
+			fragmentShader: outerFragment,
+			side: DoubleSide,
+			uniforms: {
+				uTime: { value: 0 },
+				uResolution: { value: new Vector2() },
+				uDisplace: { value: 2 },
+				uSpread: { value: 1.2 },
+				uNoise: { value: 16 },
+				tCube: { value: null },
+			},
+		});
+
+		this.mesh2 = new Mesh(geometry2, material2);
+		this.scene.add(this.mesh2);
+	}
+
+	events() {
+		window.addEventListener("resize", this.handleResize, false);
+		requestAnimationFrame(this.handleRAF);
+	}
+
+	handleResize = () => {
+		this.renderer.setSize(this.el.offsetWidth, this.el.offsetHeight);
+		this.composer.setSize(this.el.offsetWidth, this.el.offsetHeight);
+	};
+
+	handleRAF = (t: number) => {
+		requestAnimationFrame(this.handleRAF);
+		// @ts-ignore
+		this.mesh.material.uniforms.uTime.value = t / 3000;
+
+		this.mesh2.visible = false;
+		this.cubeCamera.update(this.renderer, this.scene);
+		// @ts-ignore
+		this.mesh2.material.uniforms.tCube.value = this.cubeRenderTarget.texture;
+		this.mesh2.visible = true;
+
+		this.renderer.render(this.scene, this.camera);
+		this.composer.render();
+		this.controls.update();
+	};
+}
+
+export default BallScene;
